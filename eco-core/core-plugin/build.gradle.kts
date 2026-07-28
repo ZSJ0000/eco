26e38e3e9578cb2f435a7703427d2f66abc83479
ZSJ0000 <z01-0@outlook.com>
2026-07-28 10:37:33 +0000

    Fix Eco 2026.30 build: remove unavailable optional dependencies

--- eco-core/core-plugin/build.gradle.kts
+++ eco-core/core-plugin/build.gradle.kts
@@ -114,13 +114,10 @@ dependencies {
     compileOnly("com.denizenscript:denizen:1.3.0-SNAPSHOT") {
         exclude(group = "*", module = "*")
     }
-    compileOnly("com.iridium:IridiumSkyblock:4.1.2")
     compileOnly("net.william278.huskclaims:huskclaims-bukkit:1.5.10")
     compileOnly("net.william278.husktowns:husktowns-bukkit:3.1.4")
     compileOnly("com.github.jojodmo:ItemBridge:b0054538c1")
     compileOnly("su.nightexpress.excellenteconomy:ExcellentEconomy:2.8.0")
-    compileOnly("su.nightexpress.nightcore:main:2.15.3")
-    compileOnly("su.nightexpress.excellentshop:Core:4.22.0")
     compileOnly("dev.kitteh:factions:4.4.0")
 
     compileOnly(fileTree("../../lib") {
